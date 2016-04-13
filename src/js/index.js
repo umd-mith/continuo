2295174a5d465026d37ea76a7c7993f8c2c593fb
raffazizzi <raffaeleviglianti@gmail.com>
2016-04-13 10:23:18 -0400

    Created a file upload system and button

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import VerovioInteractionView from './views/verovioInteractionView';
 import MEIdata from './data/model-MEIdata';
 import Events from './utils/backbone-events';
 import extend_vrv from './utils/verovio-ext';
+import FileUploadComponent from './components/fileupload';
 import 'xmldom';
 
 // NOTES
@@ -14,20 +15,7 @@ class Continuo extends Backbone.View {
     initialize(options) {
         this.mei = options.mei;
         this.listenTo(Events, 'component:emaBox', this.updateEmaBox);
-    }
-
-    getMEIdata(cb){
-        $.get(this.mei, (data) => { 
-            let doc = new DOMParser().parseFromString(data, 'text/xml');
-            this.MEIdata = new MEIdata(
-                {"doc": doc, 
-                 "string": data
-                });
-            this.MEIdata.generate_ids();
-            if (cb) {
-                cb();
-            }
-        }, 'text');
+        this.listenTo(Events, 'addFile', this.addFile);
     }
   
     updateEmaBox(expr){
@@ -35,46 +23,51 @@ class Continuo extends Backbone.View {
         $(".cnt-emabox").text(expr);
     }
 
-    render(){
-        if (this.MEIdata) { 
-            let container = $("<div class='cnt-container'></div>");
-            this.$el.append(container);
+    addFile(textData) {
+        // Create score
 
-            // Create EMA floating box
-            let emaBox = $("<div class='cnt-emabox'></div>");
-            container.append(emaBox);
+        let container = this.$el.find(".cnt-container");
 
-            // Create score
-
-            // Sadly, importing Verovio crashes babelify, 
-            // so we assume it's globally available
-            // i.e. verovio must be defined.
-            let vrvToolkit = new verovio.toolkit();
-            let scale = 50;
-            let options = JSON.stringify({
-                pageWidth: this.$el.width() * 100 / scale,
-                ignoreLayout: 1,
-                adjustPageHeight: 1,
-                border: 50,
-                scale: scale
+        let doc = new DOMParser().parseFromString(textData["string"], 'text/xml');
+        this.MEIdata = new MEIdata(
+            {"doc": doc, 
+             "string": textData["string"]
             });
-            vrvToolkit.setOptions(options);
-            let mei = this.MEIdata.get("string");
-            vrvToolkit.loadData( mei + "\n", "" );
-            let pgs = vrvToolkit.getPageCount();
-            for (let page of Array.from(new Array(pgs), (x,i) => i)) {
-                let svg = vrvToolkit.renderPage(page+1);
 
-                let ext_svg = extend_vrv(svg);
+        // Sadly, importing Verovio crashes babelify, 
+        // so we assume it's globally available
+        // i.e. verovio must be defined.
+        let vrvToolkit = new verovio.toolkit();
+        let scale = 50;
+        let options = JSON.stringify({
+            pageWidth: this.$el.width() * 100 / scale,
+            ignoreLayout: 1,
+            adjustPageHeight: 1,
+            border: 50,
+            scale: scale
+        });
+        vrvToolkit.setOptions(options);
+        let mei = textData["string"];
+        vrvToolkit.loadData( mei + "\n", "" );
+        let pgs = vrvToolkit.getPageCount();
+        for (let page of Array.from(new Array(pgs), (x,i) => i)) {
+            let svg = vrvToolkit.renderPage(page+1);
 
-                container.append(ext_svg);
-            }
-            new VerovioInteractionView({"el": container, "model": this.MEIdata});
+            let ext_svg = extend_vrv(svg);
+            container.append(ext_svg);
         }
-        else {
-            // bind(this) preserves the context
-            this.getMEIdata(this.render.bind(this));
-        }
+        new VerovioInteractionView({"el": container, "model": this.MEIdata});
+    }
+
+    render(){        
+        let container = $("<div class='cnt-container'></div>");
+        this.$el.append(container);
+
+        // Create EMA floating box
+        let emaBox = $("<div class='cnt-emabox'></div>");
+        container.append(emaBox);
+
+        new FileUploadComponent({"el":container});
     }
 
 }
---
+++ src/js/templates/loadfile-tpl.js
@@ -0,0 +1,3 @@
+export default `
+<div class='cnt-uploadbox'><button type='button' class='btn-file'>Load file<input type='file'></button></div>
+`
