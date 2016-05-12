eba23c9f918d56fc7ce77d993a58c6426d1b69eb
raffazizzi <raffaeleviglianti@gmail.com>
2016-05-12 13:23:19 -0400

    Crude copy button

--- src/js/components/EMAexpr.js
+++ src/js/components/EMAexpr.js
@@ -27,44 +27,8 @@ class EMAExprComponent extends Backbone.View {
 
     copyExpr(e) {
         e.preventDefault();
-
-        // var textArea = document.createElement("textarea");
-
-        // // Place in top-left corner of screen regardless of scroll position.
-        // textArea.style.position = 'fixed';
-        // textArea.style.top = 0;
-        // textArea.style.left = 0;
-
-        // // Ensure it has a small width and height. Setting to 1px / 1em
-        // // doesn't work as this gives a negative w/h on some browsers.
-        // textArea.style.width = '2em';
-        // textArea.style.height = '2em';
-
-        // // We don't need padding, reducing the size if it does flash render.
-        // textArea.style.padding = 0;
-
-        // // Clean up any borders.
-        // textArea.style.border = 'none';
-        // textArea.style.outline = 'none';
-        // textArea.style.boxShadow = 'none';
-
-        // // Avoid flash of white box if rendered for any reason.
-        // textArea.style.background = 'transparent';
-        // textArea.value = this.$el.find("cnt-emaexpr").text();
-
-        // document.body.appendChild(textArea);
-
-        // textArea.select();
-
-        // try {
-        //     var successful = document.execCommand('copy');
-        //     var msg = successful ? 'successful' : 'unsuccessful';
-        //     alert('Copying text was ' + msg);
-        // } catch (err) {
-        //     alert('Oops, unable to copy');
-        // }
-
-        // document.body.removeChild(textArea);
+        let url = "/" + encodeURIComponent(this.MEIurl) + "/" + this.expr;
+        window.prompt ("Copy to clipboard: Ctrl+C, Enter", url);
     }
 
     updateEmaBox(expr) {
